d99397e8876300d64fa064926a2628f6cc2dedd7
Dushko Fedor <fdsaq1234@ya.ru>
2022-09-16 23:31:04 +0300

    fix db create

--- Main_project/database.py
+++ Main_project/database.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 
 db = SQLAlchemy()
 
@@ -15,7 +16,18 @@ class ProductCard(db.Model):
 
     # def __init__(self, text, tags):
     #     self.text = text.strip()
-    
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fridge_catalog.db'
+    db.init_app(app)
+    return app
+
+
+if __name__ == "__main__":
+
+    db.create_all(app=create_app())
 
 
 
